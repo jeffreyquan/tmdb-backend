2a9e6e456815a7dfddbe0db3a185e5a1210d9cfa
Jeffrey <jeffrey.quan@hotmail.com>
2021-09-25 00:07:23 +1000

    feat: add configuration for swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
+
+  const options = new DocumentBuilder()
+    .setTitle('tmdb')
+    .setDescription('The Movie Database')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(5000);
 }
 bootstrap();
