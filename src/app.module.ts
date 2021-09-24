bc52504ec625b815b63e56834681c9a51fc32c5a
Jeffrey <jeffrey.quan@hotmail.com>
2021-09-25 00:07:06 +1000

    feat: add configuration for postgres and typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MoviesModule } from './movies/movies.module';
+import { ActorsModule } from './actors/actors.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { getConnectionOptions } from 'typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      useFactory: async () =>
+        Object.assign(await getConnectionOptions(), {
+          autoLoadEntities: true,
+          synchronize: true,
+        }),
+    }),
+    MoviesModule,
+    ActorsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
