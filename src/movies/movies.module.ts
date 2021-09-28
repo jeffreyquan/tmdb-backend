a414c57f7ffb0661c96e20035414a13e7fc68b88
Jeffrey <jeffrey.quan@hotmail.com>
2021-09-29 01:58:55 +1000

    feat: add list and genre entity to movies module

--- src/movies/movies.module.ts
+++ src/movies/movies.module.ts
@@ -3,9 +3,11 @@ import { Module } from '@nestjs/common';
 import { MoviesService } from './movies.service';
 import { Movie } from './entities/movie.entity';
 import { MoviesController } from './movies.controller';
+import { Genre } from './entities/genre.entity';
+import { List } from 'src/lists/entities/list.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Movie])],
+  imports: [TypeOrmModule.forFeature([Movie, Genre, List])],
   controllers: [MoviesController],
   providers: [MoviesService],
 })
