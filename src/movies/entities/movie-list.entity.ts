d7f7549f67c8d022e9ec2fa41546ca25df6a7102
Jeffrey <jeffrey.quan@hotmail.com>
2021-09-29 01:56:48 +1000

    feat: add movie-list entity

---
+++ src/movies/entities/movie-list.entity.ts
@@ -0,0 +1,26 @@
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { List } from 'src/lists/entities/list.entity';
+import { Movie } from './movie.entity';
+
+@Entity()
+export class MovieList {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @ManyToOne(() => List)
+  @JoinColumn({ name: 'list_id' })
+  list: List;
+
+  @ManyToOne(() => Movie)
+  @JoinColumn({ name: 'movie_id' })
+  movie: Movie;
+
+  @Column('boolean', { default: false })
+  has_watched: boolean;
+}
