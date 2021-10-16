0a517bda68f32170cc056a50adacd42f8a46e19e
Jeffrey <jeffrey.quan@hotmail.com>
2021-10-16 14:40:45 +1100

    test: add e2e test for searching for movies

---
+++ test/movie/mock-data.ts
@@ -0,0 +1,203 @@
+export const mockSearchResponse = {
+  page: 1,
+  results: [
+    {
+      backdrop: '/pajKyahlPPggk0k5LiA2v4kwWqn.jpg',
+      id: 671,
+      overview:
+        "Harry Potter has lived under the stairs at his aunt and uncle's house his whole life. But on his 11th birthday, he learns he's a powerful wizard—with a place waiting for him at the Hogwarts School of Witchcraft and Wizardry. As he learns to harness his newfound powers with the help of the school's kindly headmaster, Harry uncovers the truth about his parents' deaths—and about the villain who's to blame.",
+      poster:
+        'https://image.tmdb.org/t/p/w500//wuMc08IPKEatf9rnMNXvIDxqP4W.jpg',
+      releaseDate: '2001-11-16',
+      title: "Harry Potter and the Philosopher's Stone",
+    },
+    {
+      backdrop: '/kT8bDEAgEYBKhRJtqM97qTw6uRW.jpg',
+      id: 767,
+      overview:
+        'As Lord Voldemort tightens his grip on both the Muggle and wizarding worlds, Hogwarts is no longer a safe haven. Harry suspects perils may even lie within the castle, but Dumbledore is more intent upon preparing him for the final battle fast approaching. Together they work to find the key to unlock Voldemorts defenses and to this end, Dumbledore recruits his old friend and colleague Horace Slughorn, whom he believes holds crucial information. Even as the decisive showdown looms, romance blossoms for Harry, Ron, Hermione and their classmates. Love is in the air, but danger lies ahead and Hogwarts may never be the same again.',
+      poster:
+        'https://image.tmdb.org/t/p/w500//z7uo9zmQdQwU5ZJHFpv2Upl30i1.jpg',
+      releaseDate: '2009-07-07',
+      title: 'Harry Potter and the Half-Blood Prince',
+    },
+    {
+      backdrop: '/8f9dnOtpArDrOMEylpSN9Sc6fuz.jpg',
+      id: 674,
+      overview:
+        "When Harry Potter's name emerges from the Goblet of Fire, he becomes a competitor in a grueling battle for glory among three wizarding schools—the Triwizard Tournament. But since Harry never submitted his name for the Tournament, who did? Now Harry must confront a deadly dragon, fierce water demons and an enchanted maze only to find himself in the cruel grasp of He Who Must Not Be Named.",
+      poster:
+        'https://image.tmdb.org/t/p/w500//fECBtHlr0RB3foNHDiCBXeg9Bv9.jpg',
+      releaseDate: '2005-11-16',
+      title: 'Harry Potter and the Goblet of Fire',
+    },
+    {
+      backdrop: '/1stUIsjawROZxjiCMtqqXqgfZWC.jpg',
+      id: 672,
+      overview:
+        'Cars fly, trees fight back, and a mysterious house-elf comes to warn Harry Potter at the start of his second year at Hogwarts. Adventure and danger await when bloody writing on a wall announces: The Chamber Of Secrets Has Been Opened. To save Hogwarts will require all of Harry, Ron and Hermione’s magical abilities and courage.',
+      poster:
+        'https://image.tmdb.org/t/p/w500//sdEOH0992YZ0QSxgXNIGLq1ToUi.jpg',
+      releaseDate: '2002-11-13',
+      title: 'Harry Potter and the Chamber of Secrets',
+    },
+    {
+      backdrop: '/obKmfNexgL4ZP5cAmzdL4KbHHYX.jpg',
+      id: 673,
+      overview:
+        'Year three at Hogwarts means new fun and challenges as Harry learns the delicate art of approaching a Hippogriff, transforming shape-shifting Boggarts into hilarity and even turning back time. But the term also brings danger: soul-sucking Dementors hover over the school, an ally of the accursed He-Who-Cannot-Be-Named lurks within the castle walls, and fearsome wizard Sirius Black escapes Azkaban. And Harry will confront them all.',
+      poster:
+        'https://image.tmdb.org/t/p/w500//aWxwnYoe8p2d2fcxOqtvAtJ72Rw.jpg',
+      releaseDate: '2004-05-31',
+      title: 'Harry Potter and the Prisoner of Azkaban',
+    },
+    {
+      backdrop: '/bsaA9qtIR6VTDQykXdBzJc8V5rS.jpg',
+      id: 675,
+      overview:
+        'The rebellion begins! Lord Voldemort has returned, but the Ministry of Magic is doing everything it can to keep the wizarding world from knowing the truth – including appointing Ministry official Dolores Umbridge as the new Defence Against the Dark Arts professor at Hogwarts. When Umbridge refuses to teach practical defensive magic, Ron and Hermione convince Harry to secretly train a select group of students for the wizarding war that lies ahead. A terrifying showdown between good and evil awaits in this enthralling film version of the fifth novel in J.K. Rowling’s Harry Potter series. Prepare for battle!',
+      poster:
+        'https://image.tmdb.org/t/p/w500//s836PRwHkLjrOJrfW0eo7B4NJOf.jpg',
+      releaseDate: '2007-06-28',
+      title: 'Harry Potter and the Order of the Phoenix',
+    },
+    {
+      backdrop: '/n5A7brJCjejceZmHyujwUTVgQNC.jpg',
+      id: 12445,
+      overview:
+        "Harry, Ron and Hermione continue their quest to vanquish the evil Voldemort once and for all. Just as things begin to look hopeless for the young wizards, Harry discovers a trio of magical objects that endow him with powers to rival Voldemort's formidable skills.",
+      poster:
+        'https://image.tmdb.org/t/p/w500//da22ZBmrDOXOCDRvr8Gic8ldhv4.jpg',
+      releaseDate: '2011-07-07',
+      title: 'Harry Potter and the Deathly Hallows: Part 2',
+    },
+    {
+      backdrop: '/AqLcLsGGTzAjm3pCCq0CZCQrp6m.jpg',
+      id: 12444,
+      overview:
+        "Harry, Ron and Hermione walk away from their last year at Hogwarts to find and destroy the remaining Horcruxes, putting an end to Voldemort's bid for immortality. But with Harry's beloved Dumbledore dead and Voldemort's unscrupulous Death Eaters on the loose, the world is more dangerous than ever.",
+      poster: 'https://image.tmdb.org/t/p/w500//f1geQ0I9UETRF1ed30voZfw8zT.jpg',
+      releaseDate: '2010-10-17',
+      title: 'Harry Potter and the Deathly Hallows: Part 1',
+    },
+    {
+      backdrop: '/4LOxoeZWt5wr9mooxEZRFK96lDd.jpg',
+      id: 483898,
+      overview:
+        "To mark the release two weeks ago of the eighth and final movie in the series, Robbie Coltrane narrates a countdown of the movie franchise's best moments. From Harry's first meeting with Ron and Hermione aboard the Hogwarts Express through to magical mysteries.",
+      poster:
+        'https://image.tmdb.org/t/p/w500//g1xiBoLD6v3ZaXPa4QtuXiQeYKW.jpg',
+      releaseDate: '2011-07-27',
+      title: '50 Greatest Harry Potter Moments',
+    },
+    {
+      backdrop: '/5kex1QQCRjNFKbsaFLaX4j3IDKa.jpg',
+      id: 984,
+      overview:
+        "When a madman dubbed 'Scorpio' terrorizes San Francisco, hard-nosed cop, Harry Callahan – famous for his take-no-prisoners approach to law enforcement – is tasked with hunting down the psychopath. Harry eventually collars Scorpio in the process of rescuing a kidnap victim, only to see him walk on technicalities. Now, the maverick detective is determined to nail the maniac himself.",
+      poster: 'https://image.tmdb.org/t/p/w500//UHxxkYe9tRdiPu0JFgcEL5hmQ4.jpg',
+      releaseDate: '1971-07-14',
+      title: 'Dirty Harry',
+    },
+    {
+      backdrop: '/AhmRmdlu7GYdiucfZzVCGyP8ICf.jpg',
+      id: 25941,
+      overview:
+        "An elderly ex-serviceman and widower looks to avenge his best friend's murder by doling out his own form of justice.",
+      poster:
+        'https://image.tmdb.org/t/p/w500//zthprdjuK7vN4ABBGQXo8RF7qYB.jpg',
+      releaseDate: '2009-11-11',
+      title: 'Harry Brown',
+    },
+    {
+      backdrop: '/cXU4zg2aKglQrMOLokHLbnxwkGV.jpg',
+      id: 639,
+      overview:
+        "During their travel from Chicago to New York, Harry and Sally debate whether or not sex ruins a friendship between a man and a woman. Eleven years later, and they're still no closer to finding the answer.",
+      poster:
+        'https://image.tmdb.org/t/p/w500//3wkbKeowUp1Zpkw1KkBqMWbt0P9.jpg',
+      releaseDate: '1989-01-12',
+      title: 'When Harry Met Sally...',
+    },
+    {
+      backdrop: '/emaq05o59ae7ooMrYGi6y6zk10p.jpg',
+      id: 8989,
+      overview:
+        'Returning from a hunting trip in the forest, the Henderson family\'s car hits an animal in the road. At first they fear it was a man, but when they examine the "body" they find it\'s a "bigfoot". They think it\'s dead so they decide to take it home (there could be some money in this). As you guessed, it isn\'t dead. Far from being the ferocious monster they fear "Harry" to be, he\'s a friendly giant.',
+      poster:
+        'https://image.tmdb.org/t/p/w500//reHlE6G4P1Tsd75REdD1ruhpKnm.jpg',
+      releaseDate: '1987-06-05',
+      title: 'Harry and the Hendersons',
+    },
+    {
+      backdrop: '/coJLkvK5KZQoMl8vGqcpgAZYroi.jpg',
+      id: 2639,
+      overview:
+        'This film tells the story of a successful writer called Harry Block, played by Allen himself, who draws inspiration from people he knows in real-life, and from events that happened to him, sometimes causing these people to become alienated from him as a result.',
+      poster:
+        'https://image.tmdb.org/t/p/w500//2ZkTBt5wunAIIEx6UFtxxLdzkKM.jpg',
+      releaseDate: '1997-12-12',
+      title: 'Deconstructing Harry',
+    },
+    {
+      backdrop: '/hNQ0R4wSaXTUPUQti8FtKLf1HQ6.jpg',
+      id: 11718,
+      overview:
+        "Harry Crumb is a bumbling and inept private investigator who is hired to solve the kidnapping of a young heiress which he's not expected to solve because his employer is the mastermind behind the kidnapping.",
+      poster:
+        'https://image.tmdb.org/t/p/w500//g4pVY53VfjFDZGbOow2ii1l2Zwh.jpg',
+      releaseDate: '1989-02-03',
+      title: "Who's Harry Crumb?",
+    },
+    {
+      backdrop: null,
+      id: 833629,
+      overview:
+        'Dive deeper into the World of Harry Potter by focusing on the characters in the film and going behind the scenes with filmmakers.',
+      poster:
+        'https://image.tmdb.org/t/p/w500//cKkb3UlCQl86SYEYi6aGtfGnFbf.jpg',
+      releaseDate: '2009-12-08',
+      title: 'Creating the World of Harry Potter, Part 2: Characters',
+    },
+    {
+      backdrop: '/kagW7NjLnnWzF7BFX8CfNUobQWM.jpg',
+      id: 27091,
+      overview:
+        "Harry is a 23-year-old former boy band idol who is watching his younger brother Max, 16, follow in his footsteps. Harry has detoured on his way to a Japanese concert tour to escort Max on a long-promised camping adventure. Their trip begins on a note of camaraderie but quickly turns serious as old wounds resurface, forcing them to come to terms with their dysfunctional past--Harry's drinking problems, his disconnection from the family, and, most of all, his relationship with Max and the emotional dependency that keeps them from moving into adulthood.",
+      poster:
+        'https://image.tmdb.org/t/p/w500//nIamsnmqYgRLuosJFlWGiCMXS8A.jpg',
+      releaseDate: '2004-01-17',
+      title: 'Harry + Max',
+    },
+    {
+      backdrop: null,
+      id: 881554,
+      overview: '',
+      poster: 'https://image.tmdb.org/t/p/w500//APuWMuQGqHZMG236ZTsMhNpUXF.jpg',
+      releaseDate: '2021-08-10',
+      title: 'Harry Potter: Different Perspective',
+    },
+    {
+      backdrop: '/mjCYpJXCBYDZgMr1U1KZcFgFDfC.jpg',
+      id: 482408,
+      overview:
+        'A thrilling journey through legends, belief and folklore, this film goes behind the scenes with the British Library as they search to tell that story through objects in their collection, in an ambitious new exhibition: Harry Potter: A History Of Magic.  J.K. Rowling, who is lending unseen manuscripts, drawings and drafts from her private archives (which will sit alongside treasures from the British Library, as well as original drafts and drawings from Jim Kay) talks about some of the personal items she has lent to the exhibition and gives new insight into her writing, looking at some of the objects from the exhibition that have fired her imagination.',
+      poster:
+        'https://image.tmdb.org/t/p/w500//geike7VgTrftxwzcvND4dRdZdAv.jpg',
+      releaseDate: '2017-10-28',
+      title: 'Harry Potter: A History Of Magic',
+    },
+    {
+      backdrop: '/kqNOguuCc9fQuWrzv3LE6L1I4J1.jpg',
+      id: 11219,
+      overview:
+        "Trouble erupts in a small, quiet New England town when a man's body is found in the woods. The problem is that almost everyone in town thinks that they had something to do with his death.",
+      poster:
+        'https://image.tmdb.org/t/p/w500//uUXLq7fEG3hI46ZFMZzgHj11S6S.jpg',
+      releaseDate: '1955-10-03',
+      title: 'The Trouble With Harry',
+    },
+  ],
+  total_pages: 24,
+  total_results: 478,
+};
--- test/movie/movies.e2e-spec.ts
+++ test/movie/movies.e2e-spec.ts
@@ -1,12 +1,18 @@
+import { mockSearchResponse } from './mock-data';
+import { SearchQueryDto } from './../../src/movies/dto/search-query.dto';
 import { Test, TestingModule } from '@nestjs/testing';
 import { HttpStatus, INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { MoviesModule } from 'movies/movies.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { getConnectionOptions } from 'typeorm';
+import { MoviesService } from 'movies/movies.service';
 
 describe('[Feature] Movies - /movies', () => {
   let app: INestApplication;
+  const moviesService = {
+    search: () => JSON.stringify(mockSearchResponse),
+  };
 
   beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -20,7 +26,10 @@ describe('[Feature] Movies - /movies', () => {
             }),
         }),
       ],
-    }).compile();
+    })
+      .overrideProvider(MoviesService)
+      .useValue(moviesService)
+      .compile();
 
     app = moduleFixture.createNestApplication();
 
@@ -29,7 +38,14 @@ describe('[Feature] Movies - /movies', () => {
 
   it('Searches and returns results [GET /movies/search?query=${query}]', () => {
     return request(app.getHttpServer())
-      .get('/movies/search?query=harry')
-      .expect(HttpStatus.OK);
+      .get('/movies/search')
+      .query({
+        query: 'harry',
+      } as SearchQueryDto)
+      .expect(HttpStatus.OK, moviesService.search());
+  });
+
+  afterAll(async () => {
+    await app.close();
   });
 });
