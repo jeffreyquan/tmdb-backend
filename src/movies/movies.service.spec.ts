90f8ed97545c127a1ef99bf7c61de70d695a2e14
Jeffrey <jeffrey.quan@hotmail.com>
2021-10-17 02:42:00 +1100

    test: add unit test for finding one movie

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -1,18 +1,70 @@
+import { MaybeMockedDeep } from 'ts-jest/dist/utils/testing';
+import { HttpService } from '@nestjs/axios';
+import { Repository } from 'typeorm';
 import { Test, TestingModule } from '@nestjs/testing';
 import { MoviesService } from './movies.service';
+import { Movie } from './entities/movie.entity';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { GenresService } from 'genres/genres.service';
+import { DirectorsService } from 'directors/directors.service';
+import { ActorsService } from 'actors/actors.service';
+import { mockMovie } from 'mocks/mock-data';
 
 describe('MoviesService', () => {
   let service: MoviesService;
+  let repo: Repository<Movie>;
+  let spyHttpService: MaybeMockedDeep<HttpService>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [MoviesService],
+      providers: [
+        MoviesService,
+        {
+          provide: getRepositoryToken(Movie),
+          useValue: {
+            findOne: jest.fn().mockReturnValue(mockMovie),
+          },
+        },
+        {
+          provide: GenresService,
+          useValue: {},
+        },
+        {
+          provide: DirectorsService,
+          useValue: {},
+        },
+        {
+          provide: ActorsService,
+          useValue: {},
+        },
+        {
+          provide: HttpService,
+          useFactory: () => ({
+            get: jest.fn(),
+          }),
+        },
+      ],
     }).compile();
 
     service = module.get<MoviesService>(MoviesService);
+    repo = module.get<Repository<Movie>>(getRepositoryToken(Movie));
+    spyHttpService = module.get(HttpService);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('findOne', () => {
+    it('should return a single movie', () => {
+      const repoSpy = jest.spyOn(repo, 'findOne');
+      expect(service.findOne(123)).resolves.toEqual(mockMovie);
+      expect(repoSpy).toBeCalledWith(
+        123,
+        expect.objectContaining({
+          relations: ['actors', 'director', 'genres', 'ratings'],
+        }),
+      );
+    });
   });
 });
