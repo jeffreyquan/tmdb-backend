b1343a88f7666e28ae012fd5d2bb635b0b8cca37
Jeffrey <jeffrey.quan@hotmail.com>
2021-11-16 23:22:41 +1100

    (refactor) tmdb headers config

--- src/config/tmdb.config.ts
+++ src/config/tmdb.config.ts
@@ -1,6 +1,10 @@
-export default () => ({
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+export const tmdbApiHeaders = {
   headers: {
     Authorization: `Bearer ${process.env.TMDB_API_TOKEN}`,
     'Content-Type': 'application/json;charset=utf-8',
   },
-});
+};
