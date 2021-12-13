25c496cabd0180b2000195a855f743560185b4a7
Jeffrey <jeffrey.quan@hotmail.com>
2021-12-13 23:24:00 +1100

    feat: add email verified field to create user

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -2,10 +2,6 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsString } from 'class-validator';
 
 export class CreateUserDto {
-  @ApiProperty({ description: 'Auth0 ID' })
-  @IsString()
-  readonly id: string;
-
   @ApiProperty({ description: 'Username' })
   @IsString()
   readonly username: string;
@@ -13,4 +9,8 @@ export class CreateUserDto {
   @ApiProperty({ description: 'User email' })
   @IsEmail()
   readonly email: string;
+
+  @ApiProperty({ description: 'User email verified' })
+  @IsEmail()
+  readonly emailVerified: boolean;
 }
